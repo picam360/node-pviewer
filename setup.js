f34de86fe8b9ad4c84de6d345ff00c81d77c28bb
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2023-07-27 09:36:14 +0900

    v0.4.7

--- setup.js
+++ setup.js
@@ -14,7 +14,7 @@ try{
 }
 
 try{
-	execSync('git clone --depth 1 https://github.com/picam360/pviewer.git www -b v0.28', {cwd : __dirname});
+	execSync('git clone --depth 1 https://github.com/picam360/pviewer.git www -b v0.29', {cwd : __dirname});
 }catch(err){
 	console.log("error on git:" + err);
 }
