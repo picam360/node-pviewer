8a01c7cdad04feaaae412efcc64c0b1a9d938fcd
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-09-03 17:32:12 +0900

    drive-path

--- plugins/auto-drive/drive-path-from-recorder.js
+++ plugins/auto-drive/drive-path-from-recorder.js
@@ -5,44 +5,107 @@ module.exports = {
 		const fs = require("fs");
 		const path = require('path');
 		const nmea = require('nmea-simple');
+		const xml2js = require('xml2js');
 
 		let m_options = {
 			"waypoint_threshold_m" : 10,
 		};
 
-		let m_active_recorder_path = "";
-		let m_active_recorder_path_watch = null;
+		let m_active_recorder_filepath = "";
+		let m_active_recorder_filepath_watch = null;
+		let m_active_drive_path = {};
+		let m_active_drive_path_buffer = {};
 		
-		function set_active_recorder_path(data) {
-			m_active_recorder_path = data;
-			if(m_active_recorder_path_watch){
-				m_active_recorder_path_watch.close();
-				m_active_recorder_path_watch = null;
-			}
+		function read_pif_meta(filename, callback) {
+			try{
 
-			fs.readdir(m_active_recorder_path, { withFileTypes: true }, (err, entries) => {
+				fs.readFile(filename, (err, data) => {
+					if (err) throw err;
+	
+					// 最初の2バイトを取得
+					const header = data.slice(0, 2).toString('utf-8');
+					if (header !== 'PI') {
+						throw new Error('Invalid file format');
+					}
+	
+					// 次の4バイトでサイズを取得（ビッグエンディアン）
+					const xmlSize = data.readUInt16BE(2);
+	
+					// XMLデータを取得してパース
+					const xmlData = data.slice(4, 4 + xmlSize).toString('utf-8');
+					xml2js.parseString(xmlData, (err, result) => {
+						if (err) throw err;
+	
+						// METASIZE属性を取得
+						const metaSize = parseInt(result["picam360:image"].$.meta_size, 10);
+	
+						// METASIZE分の文字列を読み込む
+						const metaString = data.slice(4 + xmlSize, 4 + xmlSize + metaSize).toString('utf-8');
+						xml2js.parseString(metaString, (err, result) => {
+							if (err) throw err;
+							callback(filename, result["picam360:frame"].$);
+						});
+						callback(filename, metaString);
+					});
+				});
+			}catch(err){
+
+			};
+		}
+		function set_active_recorder_path(data) {
+			m_active_recorder_filepath = data;
+			if(m_active_recorder_filepath_watch){
+				m_active_recorder_filepath_watch.close();
+				m_active_recorder_filepath_watch = null;
+			}
+			m_active_drive_path = {};
+			m_active_drive_path_buffer = {};
+
+			fs.readdir(m_active_recorder_filepath, { withFileTypes: true }, (err, entries) => {
 				if (err) {
 					console.error('Error reading directory:', err);
 					return;
 				}
-				const files = [];
 				entries.forEach(entry => {
 					if (entry.isFile()) {
 						if(path.extname(entry.name) == ".pif"){
-							files.push(entry.name);
+							const fullPath = path.join(m_active_recorder_filepath, entry.name);
+							read_pif_meta(fullPath, (file_path, meta) => {
+								const fileNameWithoutExt = path.basename(file_path, path.extname(file_path));
+								m_active_drive_path_buffer[fileNameWithoutExt] = {
+									nmea : meta.nmea,
+									image : `/active_drive_path/${entry.name}.0.0.jpeg`,
+								};
+							});
 							//console.log(`File: ${entry.name}`);
 						}
 					} else if (entry.isDirectory()) {
 						//console.log(`Directory: ${entry.name}`);
 					}
 				});
-				m_active_recorder_path_watch = fs.watch(m_active_recorder_path, (eventType, filename) => {
+				setInterval(() => {
+					if(Object.keys(m_active_drive_path_buffer).length == 0){
+						return;
+					}
+					m_active_drive_path = Object.assign(m_active_drive_path, m_active_drive_path_buffer);
+					m_active_drive_path_buffer = {};
+					const client = m_plugin_host.get_redis_client();
+					if (client) {
+						client.set('pserver-active-drive-path', JSON.stringify(m_active_drive_path)).then((data) => {
+							console.log(data);
+						});
+					}
+				}, 3000);
+				m_active_recorder_filepath_watch = fs.watch(m_active_recorder_filepath, (eventType, filename) => {
 					if (eventType === 'rename' && path.extname(filename) == ".pif") {
-						const fullPath = path.join(m_active_recorder_path, filename);
-						if (fs.existsSync(fullPath) && fs.lstatSync(fullPath).isFile()) {
-							console.log(`New file added: ${filename}`);
-							files.push(filename);
-						}
+						const fullPath = path.join(m_active_recorder_filepath, filename);
+						read_pif_meta(fullPath, (file_path, meta) => {
+							const fileNameWithoutExt = path.basename(file_path, path.extname(file_path));
+							m_active_drive_path_buffer[fileNameWithoutExt] = {
+								nmea : meta.nmea,
+								image : `/active_drive_path/${entry.name}.0.0.jpeg`,
+							};
+						});
 					}
 				});
 			});
@@ -57,8 +120,8 @@ module.exports = {
 					setInterval(() => {
 						const client = m_plugin_host.get_redis_client();
 						if (client) {
-							client.get('pserver-active-recorder-path').then((data) => {
-								if(data && data != m_active_recorder_path){
+							client.get('pserver-active-recorder-filepath').then((data) => {
+								if(data && data != m_active_recorder_filepath){
 
 									try {
 										const stats = fs.statSync(data);
