f25fe3561755daebc9a8c2724f0ad1f3a646595c
hirayama.takuma <hirayama.takuma@ohmydigifab.com>
2024-09-18 16:42:47 +0900

    ups

--- docker/jetson/ups/setup_ups.sh
+++ docker/jetson/ups/setup_ups.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#if some related modules are in problem, next command would help
+#rm -rf /home/picam360/.local/lib/
+
 sudo apt-get install python3-smbus -y
 #sudo apt-get install python-smbus -y
 git clone --depth=1 https://github.com/waveshare/UPS-Power-Module
