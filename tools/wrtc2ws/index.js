321f8051d72e026d5c060340a9a9b7f1aad046c2
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-10-05 22:43:56 +0900

    wrtc2ws

--- tools/wrtc2ws/index.js
+++ tools/wrtc2ws/index.js
@@ -1,298 +1,90 @@
 
-var async = require('async');
-var fs = require("fs");
-var sprintf = require('sprintf-js').sprintf;
-var uuidgen = require('uuid/v4');
-var EventEmitter = require('eventemitter3');
-var xmlhttprequest = require('xmlhttprequest');
-global.XMLHttpRequest = xmlhttprequest.XMLHttpRequest;
+const async = require('async');
+const fs = require("fs");
+const sprintf = require('sprintf-js').sprintf;
+const uuidgen = require('uuid/v4');
+const EventEmitter = require('eventemitter3');
+const express = require('express');
+const WebSocket = require("ws");
+const http = require('http');
+const cors = require('cors');
+const jsonc = require('jsonc-parser');
+const yargs = require('yargs');
+const wrtc2ws = require("./wrtc2ws.js");
 
-var SIGNALING_HOST = "peer.picam360.com";
-// var SIGNALING_HOST = "test-peer-server.herokuapp.com";
-var SIGNALING_PORT = 443;
-var SIGNALING_SECURE = true;
-var P2P_API_KEY = "v8df88o1y4zbmx6r";
 
-var m_options = {};
-
-var Signaling = require("./signaling.js").Signaling;
-
-function start_webserver(callback) { // start up websocket server
-    console.log("websocket server starting up");
-    express_app = express();
-	express_app.use(cors());
-	express_app.use(express.json());
-    http = require('http').Server(express_app);
-
-	var https_key_filepath = 'certs/https/localhost-key.pem';
-	var https_cert_filepath = 'certs/https/localhost.pem';
-	if(m_options['https_key_filepath'] &&
-	   m_options['https_cert_filepath']){
-		if(fs.existsSync(m_options['https_key_filepath']) &&
-		   fs.existsSync(m_options['https_cert_filepath'])){
-			https_key_filepath = m_options['https_key_filepath'];
-			https_cert_filepath = m_options['https_cert_filepath'];
-		}else{
-			console.log("https key cert file not found.");
-		}
-	}
-	var https_options = {
-		key: fs.readFileSync(https_key_filepath),
-		cert: fs.readFileSync(https_cert_filepath)
-	};
-	https = require('https').Server(https_options, express_app);
-
-    express_app.use(express.static('../pviewer')); // this need be set
-	var http_port = 9001;
-	if(m_options['http_port']){
-		http_port = m_options['http_port'];
-	}
-    http.listen(http_port, function() {
-        console.log('listening http on *:' + http_port);
-    });
-
-	var https_port = 9002;
-	if(m_options['https_port']){
-		https_port = m_options['https_port'];
-	}
-    https.listen(https_port, function() {
-        console.log('listening https on *:' + https_port);
-    });
-    callback(null);
-}
-
-function start_websocket(callback) {
-    // websocket
-    var WebSocket = require("ws");
-    for(var server of [m_plugin_host.get_http(), m_plugin_host.get_https()]){
-        if(!server){
-            continue;
-        }
-        var ws = new WebSocket.Server({ server });
+const m_options = {
     
-        ws.on("connection", dc => {
-            class DataChannel extends EventEmitter {
-                constructor() {
-                    super();
-                    var self = this;
-                    dc.on('message', function(data) {
-                        self.emit('data', data);
-                    });
-                    dc.on('error', function(event) {
-                        console.log(event);
-                    });
-                    dc.on('close', function(event) {
-                        self.close();
-                    });
-                }
-                getMaxPayload() {
-                    return dc._maxPayload;
-                }
-                send(data) {
-                    if (dc.readyState == 3) {
-                        throw "already closed";
-                    }
-                    if (dc.readyState != 1) {
-                        console.log('something wrong : ' + dc.readyState);
-                        return;
-                    }
-                    if (!Array.isArray(data)) {
-                        data = [data];
-                    }
-                    try {
-                        for (var i = 0; i < data.length; i++) {
-                            dc.send(data[i]);
-                        }
-                    } catch (e) {
-                        console.log('error on dc.send');
-                        this.close();
-                        throw e;
-                    }
-                }
-                close() {
-                    dc.close();
-                    console.log('WebSocket closed');
-                    rtp_mod.remove_conn(this);
-                    this.emit('closed');
-                }
-            }
-            var conn = new DataChannel();
-            rtp_mod.add_conn(conn);
-        });
-    }
-    callback(null);
-}
-
-var connect = function() {
-    var sig_options = {
-        host: SIGNALING_HOST,
-        port: SIGNALING_PORT,
-        secure: SIGNALING_SECURE,
-        key: P2P_API_KEY,
-        local_peer_id: key,
-        iceServers: [{
-            "urls": "stun:stun.l.google.com:19302"
-        },
-        {
-            "urls": "stun:stun1.l.google.com:19302"
-        },
-        {
-            "urls": "stun:stun2.l.google.com:19302"
-        },
-        ],
-        debug: m_options.debug || 0,
-    };
-    var pc_map = {};
-    var sig = new Signaling(sig_options);
-    sig.connect(function() {
-        sig.start_ping();
-    });
-    sig.onrequestoffer = function(request) {
-        var pc = new global.window.RTCPeerConnection({
-            sdpSemantics: 'unified-plan',
-            iceServers: sig_options.iceServers,
-        });
-        pc_map[request.src] = pc;
-
-        var dc = pc.createDataChannel('data');
-        dc.onopen = function() {
-            console.log('Data channel connection success');
-            class DataChannel extends EventEmitter {
-                constructor() {
-                    super();
-                    var self = this;
-                    this.peerConnection = pc;
-                    dc.addEventListener('message', function(data) {
-                        self.emit('data', Buffer.from(new Uint8Array(data.data)));
-                    });
-                }
-                getMaxPayload() {
-                    return dc.maxRetransmits;
-                }
-                send(data) {
-                    if (dc.readyState == 'closed') {
-                        throw "already closed";
-                    }
-                    if (dc.readyState != 'open') {
-                        console.log('something wrong : ' + dc.readyState);
-                        return;
-                    }
-                    if (!Array.isArray(data)) {
-                        data = [data];
-                    }
-                    try {
-                        for (var i = 0; i < data.length; i++) {
-                            dc.send(data[i]);
-                        }
-                    } catch (e) {
-                        console.log('error on dc.send');
-                        this.close();
-                        throw e;
-                    }
-                }
-                close() {
-                    dc.close();
-                    pc.close();
-                    console.log('Data channel closed');
-                    this.emit('closed');
-                }
-            }
-            dc.DataChannel = new DataChannel();
-            rtp_mod.add_conn(dc.DataChannel);
-        }
-
-        pc.createOffer().then(function(sdp) {
-            console.log('setLocalDescription');
-            pc.setLocalDescription(sdp);
-            sig.offer(request.src, sdp);
-        }).catch(function(err) {
-            console.log('failed offering:' +
-                err);
-        });
-        pc.onicecandidate = function(event) {
-            if (event.candidate) {
-                sig.candidate(request.src, event.candidate);
-            } else {
-                // All ICE candidates have been sent
-            }
-        };
-        pc.onconnectionstatechange = function(event) {
-            console.log('peer connection state changed : ' + pc.connectionState);
-            switch (pc.connectionState) {
-                case "connected":
-                    // The connection has become fully connected
-                    break;
-                case "disconnected":
-                case "failed":
-                case "closed":
-                    console.log('peer connection closed');
-                    pc.close();
-                    dc.close();
-                    if (dc.DataChannel) {
-                        rtp_mod.remove_conn(dc.DataChannel);
-                    }
-                    break;
-            }
-        }
-    };
-    sig.onanswer = function(answer) {
-        if (pc_map[answer.src]) {
-            pc_map[answer.src].setRemoteDescription(answer.payload.sdp);
-        }
-    };
-    sig.oncandidate = function(candidate) {
-        if (pc_map[candidate.src] && candidate.payload.ice.candidate) {
-            pc_map[candidate.src].addIceCandidate(candidate.payload.ice);
-        }
-    };
-    sig.onclose = function(e) {
-        // console.log('Socket closed : dump error object below');
-        // console.dir(e);
-        setTimeout(() => {
-            console.log('Try to reconnect');
-            connect();
-        }, 1000);
-    };
 };
 
 function main() {
-    global.Blob = "blob";
-    global.File = "file";
-    global.WebSocket = require("ws");
-    global.window = require("wrtc");
-    global.window.evt_listener = [];
-    global.window.postMessage = function(message, origin) {
-        console.log(message);
-        var event = {
-            source: global.window,
-            data: message,
+    const argv = yargs
+        .option('bind', {
+            alias: 'b',
+            type: 'string',
+            array: true,
+            description: 'webrtc host',
+        })
+        .help()
+        .alias('help', 'h')
+        .argv;
+
+    const isValidPort = (value) => {
+        const port = Number(value);
+        return Number.isInteger(port) && port >= 0 && port <= 65535;
+    };
+    const bind_wrtc_and_ws = (wrtc_ws, ws) => {
+        ws.binaryType = 'arraybuffer';// blob or arraybuffer
+        ws.onopen = (event) => {
+            console.log("ws opened.");
         };
-        if (global.window.evt_listener["message"]) {
-            global.window.evt_listener["message"].forEach(function(
-                callback) {
-                callback(event);
+        ws.onmessage = (event) => {
+            wrtc_ws.send(event.data);
+        };
+        ws.onclose = (event) => {
+            console.log("ws closed.");
+        };
+
+        wrtc_ws.onopen = (event) => {
+            console.log("wrtc_ws opened.");
+        };
+        wrtc_ws.onmessage = (event) => {
+            ws.send(event.data);
+        };
+        wrtc_ws.onclose = (event) => {
+            console.log("wrtc_ws closed.");
+        };
+    };
+    for(const bind of argv.bind){
+        const nodes = bind.split(':');
+        if(isValidPort(nodes[0])){
+            const port = nodes[0];
+            const wrtckey = nodes[1];
+
+            console.log("websocket server starting up");
+            express_app = express();
+            express_app.use(cors());
+            express_app.use(express.json());
+            server = http.Server(express_app);
+            server.listen(port, function() {
+                console.log('listening http on *:' + port);
+            });
+
+            const ws = new WebSocket.Server({ server });
+            ws.on("connection", ws => {
+                wrtc2ws.start_wrtc_client(wrtckey, (wrtc_ws) => {
+                    bind_wrtc_and_ws(wrtc_ws, ws);
+                });
+            });
+        }else{
+            const port = nodes[1];
+            const wrtckey = nodes[0];
+            wrtc2ws.start_wrtc_host(wrtckey, (wrtc_ws) => {
+                var ws = new WebSocket(`http://localhost:${port}`);
+                bind_wrtc_and_ws(wrtc_ws, ws);
             });
         }
-    };
-    global.window.addEventListener = function(name, callback, bln) {
-        if (!global.window.evt_listener[name]) {
-            global.window.evt_listener[name] = [];
-        }
-        global.window.evt_listener[name].push(callback);
     }
-    var key = m_options.key || uuidgen();
-    console.log("\n\n\n");
-    console.log("webrtc key : " + key);
-    console.log("https://picam360.github.io/pviewer/?wrtc-key=" + key);
-    console.log("\n\n\n");
-    if (m_options.turn_server) {
-        m_options.iceServers.push({
-            urls: 'turn:turn.picam360.com:3478',
-            username: "picam360",
-            credential: "picam360"
-        });
-    }
-    connect();
 }
 
 if (require.main === module) {
---
+++ tools/wrtc2ws/wrtc2ws.js
@@ -0,0 +1,285 @@
+
+const xmlhttprequest = require('xmlhttprequest');
+global.XMLHttpRequest = xmlhttprequest.XMLHttpRequest;
+const WebSocket = require("ws");
+global.WebSocket = WebSocket;
+const wrtc = require("wrtc");
+global.window = wrtc;
+
+global.Blob = "blob";
+global.File = "file";
+global.window.evt_listener = [];
+global.window.postMessage = function(message, origin) {
+    console.log(message);
+    var event = {
+        source: global.window,
+        data: message,
+    };
+    if (global.window.evt_listener["message"]) {
+        global.window.evt_listener["message"].forEach(function(
+            callback) {
+            callback(event);
+        });
+    }
+};
+global.window.addEventListener = function(name, callback, bln) {
+    if (!global.window.evt_listener[name]) {
+        global.window.evt_listener[name] = [];
+    }
+    global.window.evt_listener[name].push(callback);
+}
+
+var Signaling = require("./signaling.js").Signaling;
+
+var SIGNALING_HOST = "peer.picam360.com";
+// var SIGNALING_HOST = "test-peer-server.herokuapp.com";
+var SIGNALING_PORT = 443;
+var SIGNALING_SECURE = true;
+var P2P_API_KEY = "v8df88o1y4zbmx6r";
+
+class Wrtc2Ws {
+    constructor(dataChannel) {
+        this.dataChannel = dataChannel;
+        this.readyState = this.getReadyState(); // Convert RTCDataChannel readyState to WebSocket-compatible readyState
+
+        // Event handlers
+        this.onopen = null;
+        this.onmessage = null;
+        this.onclose = null;
+        this.onerror = null;
+
+        // Map RTCDataChannel events to WebSocket-compatible ones
+        this.dataChannel.onopen = () => {
+            this.readyState = this.getReadyState();
+            if (this.onopen) {
+                this.onopen();
+            }
+        };
+
+        this.dataChannel.onmessage = (event) => {
+            if (this.onmessage) {
+                this.onmessage(event);
+            }
+        };
+
+        this.dataChannel.onclose = () => {
+            this.readyState = this.getReadyState();
+            if (this.onclose) {
+                this.onclose();
+            }
+        };
+
+        this.dataChannel.onerror = (error) => {
+            if (this.onerror) {
+                this.onerror(error);
+            }
+        };
+    }
+
+    // Send data just like WebSocket's send method
+    send(data) {
+        if (this.dataChannel.readyState === "open") {
+            this.dataChannel.send(data);
+        } else {
+            throw new Error("DataChannel is not open.");
+        }
+    }
+
+    // Close the data channel similar to WebSocket's close method
+    close() {
+        this.dataChannel.close();
+    }
+
+    // Convert RTCDataChannel readyState to WebSocket-like readyState
+    getReadyState() {
+        const stateMap = {
+            'connecting': 0, // WebSocket.CONNECTING
+            'open': 1,       // WebSocket.OPEN
+            'closing': 2,    // WebSocket.CLOSING
+            'closed': 3      // WebSocket.CLOSED
+        };
+        return stateMap[this.dataChannel.readyState];
+    }
+
+    // AddEventListener wrapper
+    addEventListener(event, listener) {
+        this.dataChannel.addEventListener(event, listener);
+    }
+
+    removeEventListener(event, listener) {
+        this.dataChannel.removeEventListener(event, listener);
+    }
+}
+
+function start_wrtc_client(p2p_uuid, callback, err_callback, options) {
+    options = options || {};
+
+    var sig_options = {
+        host: SIGNALING_HOST,
+        port: SIGNALING_PORT,
+        secure: SIGNALING_SECURE,
+        key: P2P_API_KEY,
+        iceServers : [
+                            {"urls": "stun:stun.l.google.com:19302"},
+                        {"urls": "stun:stun1.l.google.com:19302"},
+                        {"urls": "stun:stun2.l.google.com:19302"},
+                    ],
+        debug: options['debug'] || 0,
+    };
+    if (options['turn-server']) {
+        sig_options.iceServers.push({
+            urls: 'turn:turn.picam360.com:3478',
+            username: "picam360",
+            credential: "picam360"
+        });
+    }
+    var sig = new Signaling(sig_options);
+    sig.connect(function() {
+        var pc = new global.window.RTCPeerConnection({
+            sdpSemantics: 'unified-plan',
+            iceServers: options.iceServers
+        });
+
+        sig.onoffer = function(offer) {
+            pc.setRemoteDescription(offer.payload.sdp).then(function() {
+                return pc.createAnswer();
+            }).then(function(sdp) {
+                console.log('Created answer.');
+                
+                pc.setLocalDescription(sdp);
+                sig.answer(offer.src, sdp);
+            }).catch(function(err) {
+                console.log('Failed answering:' + err);
+            });
+            pc.onicecandidate = function(event) {
+                if (event.candidate) {
+                    sig.candidate(offer.src, event.candidate);
+                } else {
+                    // All ICE candidates have been sent
+                }
+            };
+            pc.ondatachannel = function(ev) {
+                console.log('Data channel is created!');
+
+                const dc = ev.channel;
+                callback(new Wrtc2Ws(dc));
+            };
+            pc.onerror = function(err) {
+                if (err.type == "peer-unavailable") {
+                    m_plugin_host.set_info("error : Could not connect " +
+                        p2p_uuid);
+                    err_callback();
+                }
+            };
+        };
+        sig.oncandidate = function(candidate) {
+            pc.addIceCandidate(candidate.payload.ice);
+        };
+        sig.request_offer(p2p_uuid);
+    });
+}
+function start_wrtc_host(p2p_uuid, callback, err_callback, options) {
+    options = options || {};
+
+    var P2P_API_KEY = "v8df88o1y4zbmx6r";
+    console.log("\n\n\n");
+    console.log("webrtc key : " + p2p_uuid);
+    console.log("https://picam360.github.io/pviewer/?wrtc-key=" + p2p_uuid);
+    console.log("\n\n\n");
+    var sig_options = {
+        host: SIGNALING_HOST,
+        port: SIGNALING_PORT,
+        secure: SIGNALING_SECURE,
+        key: P2P_API_KEY,
+        local_peer_id: p2p_uuid,
+        iceServers: [{
+            "urls": "stun:stun.l.google.com:19302"
+        },
+        {
+            "urls": "stun:stun1.l.google.com:19302"
+        },
+        {
+            "urls": "stun:stun2.l.google.com:19302"
+        },
+        ],
+        debug: options.debug || 0,
+    };
+    if (options.turn_server) {
+        options.iceServers.push({
+            urls: 'turn:turn.picam360.com:3478',
+            username: "picam360",
+            credential: "picam360"
+        });
+    }
+    var connect = function() {
+        var pc_map = {};
+        var sig = new Signaling(sig_options);
+        sig.connect(function() {
+            sig.start_ping();
+        });
+        sig.onrequestoffer = function(request) {
+            var pc = new global.window.RTCPeerConnection({
+                sdpSemantics: 'unified-plan',
+                iceServers: sig_options.iceServers,
+            });
+            pc_map[request.src] = pc;
+
+            const dc = pc.createDataChannel('data');
+            callback(new Wrtc2Ws(dc));
+
+            pc.createOffer().then(function(sdp) {
+                console.log('setLocalDescription');
+                pc.setLocalDescription(sdp);
+                sig.offer(request.src, sdp);
+            }).catch(function(err) {
+                console.log('failed offering:' +
+                    err);
+            });
+            pc.onicecandidate = function(event) {
+                if (event.candidate) {
+                    sig.candidate(request.src, event.candidate);
+                } else {
+                    // All ICE candidates have been sent
+                }
+            };
+            pc.onconnectionstatechange = function(event) {
+                console.log('peer connection state changed : ' + pc.connectionState);
+                switch (pc.connectionState) {
+                    case "connected":
+                        // The connection has become fully connected
+                        break;
+                    case "disconnected":
+                    case "failed":
+                    case "closed":
+                        console.log('peer connection closed');
+                        pc.close();
+                        dc.close();
+                        break;
+                }
+            }
+        };
+        sig.onanswer = function(answer) {
+            if (pc_map[answer.src]) {
+                pc_map[answer.src].setRemoteDescription(answer.payload.sdp);
+            }
+        };
+        sig.oncandidate = function(candidate) {
+            if (pc_map[candidate.src] && candidate.payload.ice.candidate) {
+                pc_map[candidate.src].addIceCandidate(candidate.payload.ice);
+            }
+        };
+        sig.onclose = function(e) {
+            // console.log('Socket closed : dump error object below');
+            // console.dir(e);
+            setTimeout(() => {
+                console.log('Try to reconnect');
+                connect();
+            }, 1000);
+        };
+    };
+    connect();
+}
+module.exports = {
+    start_wrtc_client,
+    start_wrtc_host
+};
