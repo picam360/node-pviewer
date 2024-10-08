670a7ebcde132adcb5061f263cfaf893e9abeaa1
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2024-10-08 14:53:53 +0900

    vslam

--- tools/auto-drive/index.js
+++ tools/auto-drive/index.js
@@ -2,6 +2,8 @@ const { J } = require('quaternion');
 console.log("create motor plugin");
 var fs = require("fs");
 const nmea = require('nmea-simple');
+const xml2js = require('xml2js');
+const { execSync } = require('child_process');
 
 var m_options = {
 	"waypoint_threshold_m" : 10,
@@ -75,25 +77,88 @@ function main() {
 				m_averaging_nmea.longitude /= m_averaging_count;
 				m_averaging_count = 0;
 
-				plugin.push_nmea(m_averaging_nmea);
+				push_nmea(m_averaging_nmea);
 			}
 		});
 
+		let tmp_img = [];
 		subscriber.subscribe('pserver-vslam-pst', (data, key) => {
-			console.log('pserver-vslam-pst', data.length);
+			if(data.length == 0 && tmp_img.length != 0){
+				if(m_drive_mode == "RECORD"){
+					if(tmp_img.length == 3){
+						const data = Buffer.concat([tmp_img[0], tmp_img[1]]);
+						const header = data.slice(0, 2).toString('utf-8');
+						if (header !== 'PI') {
+							throw new Error('Invalid file format');
+						}
+		
+						// 次の4バイトでサイズを取得（ビッグエンディアン）
+						const xmlSize = data.readUInt16BE(2);
+						const xmlData = data.slice(4, 4 + xmlSize).toString('utf-8');
+						xml2js.parseString(xmlData, (err, result) => {
+							if (err) throw err;
+		
+							const metaSize = parseInt(result["picam360:image"].$.meta_size, 10);
+							const metaString = data.slice(4 + xmlSize, 4 + xmlSize + metaSize).toString('utf-8');
+							xml2js.parseString(metaString, (err, result) => {
+								if (err) throw err;
+							});
+
+							const timestamp = result["picam360:image"].$.timestamp.replace(',', '.');
+							const pif_filepath = `${m_options.data_filepath}/waypoint_images/${timestamp}.pif`;
+							fs.writeFile(pif_filepath, data, (err) => {
+								if (err) {
+									console.error('pif file dump faild', err);
+								}
+							});
+							const jpeg_filepath = pif_filepath + ".0.0.JPEG";
+							fs.writeFile(jpeg_filepath, tmp_img[2], (err) => {
+								if (err) {
+									console.error('jpeg file dump faild', err);
+								}
+							});
+
+							if(m_options.debug){
+								console.log(`${pif_filepath} recorded.`);
+							}
+						});
+					}
+				}
+				tmp_img = [];
+			}else{
+				tmp_img.push(Buffer.from(data, 'base64'));
+			}
 		});
 	});
 }
 function command_handler(cmd) {
 	var split = cmd.split(' ');
 	switch(split[0]){
-		case "RECORD_START":
-			m_drive_mode = "RECORD";
+		case "START_RECORD":
+			{
+				const pif_dirpath = `${m_options.data_filepath}/waypoint_images`;
+				let succeeded = false;
+				if (!fs.existsSync(pif_dirpath)) {
+					fs.mkdir(pif_dirpath, { recursive: true }, (err) => {
+						if (err) {
+							console.error('mkdir faild', err);
+						} else {
+							succeeded = true;
+						}
+					});
+				}else{
+					succeeded = true;
+				}
+				if(succeeded){
+					m_drive_mode = "RECORD";
+				}
+			}
 			break;
-		case "RECORD_STOP":
+		case "STOP_RECORD":
 			m_drive_mode = "STANBY";
+			execSync('sync');
 			break;
-		case "AUTO_START":
+		case "START_AUTODRIVE":
 			m_drive_mode = "AUTO";
 			var config = split[1];
 			
@@ -105,7 +170,7 @@ function command_handler(cmd) {
 			}
 			m_auto_drive_data.path_confifg = JSON.parse(json_str);
 			break;
-		case "AUTO_STOP":
+		case "STOP_AUTODRIVE":
 			m_drive_mode = "STANBY";
 			break;
 	}
