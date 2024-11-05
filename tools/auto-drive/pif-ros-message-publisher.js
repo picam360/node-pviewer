dce54651264cc79951e8fae1d91476e053e0c256
kurio <kurio.gaku@picam360.com>
2024-11-05 16:18:14 +0900

    robot localiztion bridge

---
+++ tools/auto-drive/pif-ros-message-publisher.js
@@ -0,0 +1,117 @@
+const rosnodejs = require('rosnodejs');
+const std_msgs = rosnodejs.require('std_msgs').msg;
+
+class PifRosMessagePublisher {
+    constructor() {
+        this.isInitialized = false;
+        this.isInitializing = false;
+
+        // wheel vars
+        this.bWheelUpdate = false;
+        this.wheelLeftCount = 0;
+        this.wheelRightCount = 0;
+        this.wheelTimestampSec = null;
+
+        this.bGpsUpdate = false;
+        this.gpsX = 0;
+        this.gpsY = 0;
+        this.gpsRtksate = 0;
+        this.gpsHdop = 0;
+        this.gpsTimestampSec = null;
+        this.gpsNmea = null;
+    }
+
+    async initialize() {
+        if (this.isInitialized || this.isInitializing) {
+            return;
+        }
+        this.isInitializing = true;
+
+        try {
+            await rosnodejs.initNode('/pif_ros_message_publisher', {
+                onTheFly: true
+            });
+            this.isInitializing = false;
+            this.isInitialized = true;
+            console.log('pif_ros_message_publisher initialized successfully');
+        } catch (error) {
+            this.isInitializing = false;
+            this.isInitialized = false;
+            console.error('failed to initialize pif_ros_message_publisher:', error);
+            return;
+        }
+
+        this.nh = rosnodejs.nh;
+        this.std_msgs = rosnodejs.require('std_msgs').msg;
+
+        // Create publishers
+        this.wheelLeftPub = this.nh.advertise('/left_wheel_counts', std_msgs.Int32, {
+            queueSize: 10
+        });
+        this.wheelRightPub = this.nh.advertise('/right_wheel_counts', std_msgs.Int32, {
+            queueSize: 10
+        });
+        this.wheelPub = this.nh.advertise('/wheel_counts', std_msgs.Float64MultiArray, {
+            queueSize: 10
+        });
+        this.gpsPub = this.nh.advertise('/gps_nmea', std_msgs.String, {
+            queueSize: 10
+        });
+
+        this.publishMessages();
+    }
+
+    updateWheelCount(left, right, timestampSec) {
+        this.wheelLeftCount = left;
+        this.wheelRightCount = right;
+        this.wheelTimestampSec = timestampSec;
+        this.bWheelUpdate = true;
+    }
+
+    updateGpsPos(x, y, rtkstate, hdop, timestampSec) {
+        this.gpsX = x;
+        this.gpsY = y;
+        this.gpsRtksate = rtkstate;
+        this.gpsHdop = hdop;
+        this.timestampSec = timestampSec;
+        this.gpsTimestampSec = true;
+    }
+
+    updateGpsNmea(nmea, timestamp) {
+        this.gpsNmea = nmea;
+        this.gpsTimestampSec = timestamp;
+        this.bGpsUpdate = true;
+    }
+
+    publishMessages() {
+        if (!this.isInitialized) {
+            return;
+        }
+
+        if (this.bGpsUpdate) {
+            this.bGpsUpdate = false;
+            this.gpsPub.publish({ data: this.gpsNmea + "@" + this.gpsTimestampSec.toString() });
+        }
+
+        if (this.bWheelUpdate) {
+            this.bWheelUpdate = false;
+            const msg = {
+                layout: {
+                    dim: [{
+                        label: "data",
+                        size: 3,
+                        stride: 3
+                    }],
+                    data_offset: 0
+                },
+                data: [
+                    Number(this.wheelTimestampSec),
+                    Number(this.wheelLeftCount),
+                    Number(this.wheelRightCount)
+                ]
+            };
+            this.wheelPub.publish(msg);
+        }
+    }
+}
+module.exports = PifRosMessagePublisher;
--- tools/auto-drive/robot-localization-bridge.js
+++ tools/auto-drive/robot-localization-bridge.js
@@ -12,10 +12,11 @@ const xml_parser = new fxp.XMLParser({
 	attributeNamePrefix: "",
 });
 const pif_utils = require('./pif-utils');
+const PifRosMessagePublisher = require('./pif-ros-message-publisher');
 
 let m_options = {
-	"waypoint_threshold_m" : 10,
-	"data_filepath" : "auto-drive-waypoints"
+	"waypoint_threshold_m": 10,
+	"data_filepath": "auto-drive-waypoints"
 };
 let m_socket = null;
 let m_drive_mode = "STANBY";
@@ -24,9 +25,18 @@ let m_averaging_count = 0;
 let m_last_nmea = null;
 let m_auto_drive_waypoints = null;
 let m_auto_drive_cur = 0;
+let m_ros_msg_pub = new PifRosMessagePublisher();
 
-function auto_drive_handler(tmp_img){
-	if(tmp_img.length != 3){
+function toSec(timestampText) {
+	const [seconds, microseconds] = timestampText.split(',');
+	const secondsNumber = parseInt(seconds);
+	const microToSeconds = parseInt(microseconds) / 1000000;
+	const totalSeconds = secondsNumber + microToSeconds;
+	return  totalSeconds;
+}
+
+function auto_drive_handler(tmp_img) {
+	if (tmp_img.length != 3) {
 		return;
 	}
 
@@ -40,46 +50,54 @@ function auto_drive_handler(tmp_img){
 	const xml = data.slice(4, 4 + header_size).toString('utf-8');
 	const img_dom = xml_parser.parse(xml);
 
+	const timestamp = img_dom["picam360:image"].timestamp;
 	const meta_size = parseInt(img_dom["picam360:image"].meta_size, 10);
 	const meta = data.slice(4 + header_size, 4 + header_size + meta_size);
 	const frame_dom = xml_parser.parse(meta);
-	const current_nmea = nmea.parseNmeaSentence(frame_dom['picam360:frame']['passthrough:nmea']);
+	const nmea_str = frame_dom['picam360:frame']['passthrough:nmea'];
+	const current_nmea = nmea.parseNmeaSentence(nmea_str);
 	const current_imu = JSON.parse(frame_dom['picam360:frame']['passthrough:imu']);
 	const current_encoder = JSON.parse(frame_dom['picam360:frame']['passthrough:encoder']);
 
 	console.log(current_nmea, current_imu, current_encoder);
 
-	
+	if(!m_ros_msg_pub.isInitialized){
+		m_ros_msg_pub.initialize();
+	}
+	const timestampSec = toSec(timestamp);
+	m_ros_msg_pub.updateWheelCount(current_encoder.left, current_encoder.right, timestampSec);
+	m_ros_msg_pub.updateGpsNmea(nmea_str, timestampSec);
+	m_ros_msg_pub.publishMessages();
 }
 
 function main() {
-    const argv = yargs
-        .option('host', {
-            type: 'string',
-            default: 'localhost',
-            description: 'host',
-        })
-        .option('port', {
-            type: 'number',
-            default: 6379,
-            description: 'port',
-        })
-        .help()
-        .alias('help', 'h')
-        .argv;
+	const argv = yargs
+		.option('host', {
+			type: 'string',
+			default: 'localhost',
+			description: 'host',
+		})
+		.option('port', {
+			type: 'number',
+			default: 6379,
+			description: 'port',
+		})
+		.help()
+		.alias('help', 'h')
+		.argv;
 	const host = argv.host;
 	const port = argv.port;
 
-    const redis = require('redis');
-    const client = redis.createClient({
-        url: `redis://${host}:${port}`
-    });
-    client.on('error', (err) => {
-        console.error('redis error:', err);
+	const redis = require('redis');
+	const client = redis.createClient({
+		url: `redis://${host}:${port}`
+	});
+	client.on('error', (err) => {
+		console.error('redis error:', err);
 		m_client = null;
-    });
-    client.connect().then(() => {
-        console.log('redis connected:');
+	});
+	client.connect().then(() => {
+		console.log('redis connected:');
 		m_client = client;
 	});
 
@@ -89,10 +107,10 @@ function main() {
 
 		let tmp_img = [];
 		subscriber.subscribe('pserver-vslam-pst', (data, key) => {
-			if(data.length == 0 && tmp_img.length != 0){
+			if (data.length == 0 && tmp_img.length != 0) {
 				auto_drive_handler(tmp_img);
 				tmp_img = [];
-			}else{
+			} else {
 				tmp_img.push(Buffer.from(data, 'base64'));
 			}
 		});
@@ -100,5 +118,5 @@ function main() {
 }
 
 if (require.main === module) {
-    main();
+	main();
 }
