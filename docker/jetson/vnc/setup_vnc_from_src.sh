a35ec96bf30b283b968548e31fa157ec36807387
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-02-07 16:17:02 +0900

    tigervnc

--- docker/jetson/vnc/setup_vnc_from_src.sh
+++ docker/jetson/vnc/setup_vnc_from_src.sh
@@ -1,3 +1,13 @@
-git clone --depth=1 https://github.com/TigerVNC/tigervnc.git
-sudo apt-get install libfltk1.3-dev libxtst-dev libxdamage-dev libgmp-dev gettext
 
+sudo apt-get -y install libfltk1.3-dev libxtst-dev libxdamage-dev libgmp-dev gettext libpam0g-dev
+
+git clone --depth=1 https://github.com/TigerVNC/tigervnc.git
+cd tigervnc
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
+
+cd ../..
+rm -rf tigervnc
