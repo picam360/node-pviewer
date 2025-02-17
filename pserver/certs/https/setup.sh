4a12c602294aa790f4e18f0d467ebd0e782260c3
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-02-17 13:07:26 +0900

    mkcert

--- pserver/certs/https/setup.sh
+++ pserver/certs/https/setup.sh
@@ -1,8 +1,10 @@
-sudo apt-get -y install libnss3-tools
-curl -L "https://dl.filippo.io/mkcert/latest?for=linux/arm64" -o mkcert
-chmod +x mkcert
-sudo mv mkcert /usr/local/bin/mkcert
-mkcert --install
+if ! command -v mkcert >/dev/null 2>&1; then
+    sudo apt-get -y install libnss3-tools
+    curl -L "https://dl.filippo.io/mkcert/latest?for=linux/arm64" -o mkcert
+    chmod +x mkcert
+    sudo mv mkcert /usr/local/bin/mkcert
+    mkcert -install
+fi
 cp $(mkcert -CAROOT)/rootCA.pem ./
 mkcert localhost
 mv localhost.pem localhost.original.pem
