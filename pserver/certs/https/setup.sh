c348b66f3bc3997a50bea722427b8d17aa1b46f4
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-02-17 13:06:03 +0900

    mkcert

--- pserver/certs/https/setup.sh
+++ pserver/certs/https/setup.sh
@@ -3,6 +3,7 @@ curl -L "https://dl.filippo.io/mkcert/latest?for=linux/arm64" -o mkcert
 chmod +x mkcert
 sudo mv mkcert /usr/local/bin/mkcert
 mkcert --install
+cp $(mkcert -CAROOT)/rootCA.pem ./
 mkcert localhost
 mv localhost.pem localhost.original.pem
-cat localhost.original.pem $(mkcert -CAROOT)/rootCA.pem > localhost.pem
+cat localhost.original.pem rootCA.pem > localhost.pem
