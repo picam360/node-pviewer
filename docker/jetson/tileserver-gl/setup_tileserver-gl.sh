e129cdc8d0a543737913dd42006e90e49a25837b
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-05-01 22:05:52 +0900

    remove rclnodejs, add comment to tileserver

--- docker/jetson/tileserver-gl/setup_tileserver-gl.sh
+++ docker/jetson/tileserver-gl/setup_tileserver-gl.sh
@@ -6,4 +6,11 @@ DATA_PATH=/var/mbtiles
 sed -e "s%@DATA_PATH@%${DATA_PATH}%" tileserver-gl.service.in | sudo tee /etc/systemd/system/tileserver-gl.service
 sudo systemctl daemon-reload
 sudo systemctl restart tileserver-gl.service
-sudo systemctl enable tileserver-gl.service
+sudo systemctl enable tileserver-gl.
+
+#sudo cp your.mbtiles /var/mbtiles/
+
+#in JP60
+#/etc/docker/daemon.json
+#add "iptables": false
+#sudo systemctl restart docker
--- tools/auto-drive/npm_install.sh
+++ tools/auto-drive/npm_install.sh
@@ -1,5 +1,12 @@
 export OPENCV_INCLUDE_DIR=/usr/include/opencv4
-export OPENCV_LIB_DIR=/usr/lib/aarch64-linux-gnu
-export OPENCV_BIN_DIR=/usr/lib/aarch64-linux-gnu 
+
+#JP51
+#export OPENCV_LIB_DIR=/usr/lib/aarch64-linux-gnu
+#export OPENCV_BIN_DIR=/usr/lib/aarch64-linux-gnu 
+#JP60
+export OPENCV_LIB_DIR=/usr/lib
+export OPENCV_BIN_DIR=/usr/lib
+
 export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
+export OPENCV4NODEJS_AUTOBUILD_OPENCV_VERSION=4.1.1
 npm install
