4a082c08fc708b7d6a21b8f0537c014fc44c3c03
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-06-05 21:14:23 +0900

    vslam

--- docker/jetson/fancontrol/jetson-nano-fancontrol.sh
+++ docker/jetson/fancontrol/jetson-nano-fancontrol.sh
@@ -3,3 +3,10 @@ cd jetson-fan-ctl
 ./install.sh
 cd ..
 rm -rf jetson-fan-ctl
+
+
+#orin nx
+#sudo nano /etc/nvfancontrol.conf
+#FAN_DEFAULT_PROFILE quiet -> FAN_DEFAULT_PROFILE cool
+#sudo rm /var/lib/nvfancontrol/status
+#sudo systemctl restart nvfancontrol.service
