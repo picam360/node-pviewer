86f1a82ddea5812d27663248fa3241bbff960f50
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-11-10 12:07:52 +0900

    argus

--- pserver/app.js
+++ pserver/app.js
@@ -282,6 +282,7 @@ async.waterfall([
 		}else if(process.platform === 'linux') {
 			config_json += "        \"plugins/pcuda_remapper_st.so\",\n";
 			config_json += "        \"plugins/v4l2_capture_st.so\",\n";
+			config_json += "        \"plugins/argus_capture_st.so\",\n";
 			config_json += "        \"plugins/mjpeg_tegra_decoder_st.so\",\n";
 			config_json += "        \"plugins/v4l2_tegra_encoder_st.so\",\n";
 			config_json += "        \"plugins/mjpeg_encoder_st.so\",\n";
