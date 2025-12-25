1e866966e69126e91de4c95a67994f04f99137a0
Takuma Hirayama <hirayama.takuma@ohmydigifab.com>
2025-12-25 18:37:19 +0900

    vio

--- tools/imu-icm20948/index.js
+++ tools/imu-icm20948/index.js
@@ -29,8 +29,10 @@ const GYRO_SCALE  = Math.PI / 180 / 131; // rad/s per LSB
 let t0_hr = 0n;
 let t0_epoch_ns = 0n;
 
-function now_ns() {
-    const hr = process.hrtime.bigint();
+function now_ns(hr) {
+    if(hr === undefined){
+        hr = process.hrtime.bigint();
+    }
     let now_ns = t0_epoch_ns + (hr - t0_hr);
     const date_ns = BigInt(Date.now()) * 1_000_000n;
 
@@ -99,9 +101,19 @@ function readImu(bus) {
     return { accel, gyro };
 }
 
+function cal_norm(vec){
+    return Math.sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
+}
+
 // ======================================================
 // Main
 // ======================================================
+const ACCEL_GAIN_X = 0.99918517;
+const ACCEL_GAIN_Y = 0.99241275;
+const ACCEL_GAIN_Z = 1.00153139;
+// const ACCEL_GAIN_X = 1.0;
+// const ACCEL_GAIN_Y = 1.0;
+// const ACCEL_GAIN_Z = 1.0;
 async function main() {
     const bus = i2c.openSync(7);
     initICM20948(bus);
@@ -109,6 +121,9 @@ async function main() {
     const client = redis.createClient();
     await client.connect();
 
+    const n_block = 16;
+    let count = 0;
+    let block_imu_value;
     setInterval(() => {
         const timestamp = now_ns();
         const { accel, gyro } = readImu(bus);
@@ -117,17 +132,53 @@ async function main() {
             accel,
             gyro,
         };
-        client.publish(
-            'pserver-imu',
-            JSON.stringify(imu_value, (key, value) => {
-                if (typeof value === "number") {
-                    return Math.round(value * 1e9) / 1e9;
-                }else{
-                    return value;
-                }
-            }, 2)
-        );
-    }, 1000/500); // 500 Hz
+        count++;
+        if((count%n_block) == 1){
+            block_imu_value = imu_value;
+            block_imu_value.hr = process.hrtime.bigint();
+            return;
+        }else{
+            block_imu_value.hr += process.hrtime.bigint();
+
+            block_imu_value.accel.x += imu_value.accel.x;
+            block_imu_value.accel.y += imu_value.accel.y;
+            block_imu_value.accel.z += imu_value.accel.z;
+
+            block_imu_value.gyro.x += ACCEL_GAIN_X;
+            block_imu_value.gyro.y += ACCEL_GAIN_Y;
+            block_imu_value.gyro.z += ACCEL_GAIN_Z;
+        }
+        if((count%n_block) == 0){
+            block_imu_value.timestamp = now_ns(block_imu_value.hr / BigInt(n_block));
+            delete block_imu_value.hr;
+
+            block_imu_value.accel.x /= n_block;
+            block_imu_value.accel.y /= n_block;
+            block_imu_value.accel.z /= n_block;
+
+            block_imu_value.gyro.x /= n_block;
+            block_imu_value.gyro.y /= n_block;
+            block_imu_value.gyro.z /= n_block;
+
+            block_imu_value.accel.x *= ACCEL_GAIN_X;
+            block_imu_value.accel.y *= ACCEL_GAIN_Y;
+            block_imu_value.accel.z *= ACCEL_GAIN_Z;
+
+            const norm = cal_norm(block_imu_value.accel);
+            console.log(block_imu_value.timestamp.sec, block_imu_value.timestamp.nanosec, norm)
+
+            client.publish(
+                'pserver-imu',
+                JSON.stringify(block_imu_value, (key, value) => {
+                    if (typeof value === "number") {
+                        return Math.round(value * 1e9) / 1e9;
+                    }else{
+                        return value;
+                    }
+                }, 2)
+            );
+        }
+    }, 1000/1000); // 1000 Hz
 }
 
 if (require.main === module) {
